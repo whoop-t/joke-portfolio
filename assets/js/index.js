6adc3f571581b6d461f4858c99575c5599f77ca8
Tommy Mallis <tommymallis@gmail.com>
2021-01-30 19:19:53 -0800

    new design

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+const skills = [
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Angular',
+  'Nextjs',
+  'Redux',
+  'Node/Express',
+  'MongoDB',
+  'HTML',
+  'CSS/SCSS/JSS',
+  'BootStrap',
+  'Material'
+];
+
+const el = document.getElementById('skills-wrapper');
+skills.forEach(skill => {
+  let newEl = document.createElement('div');
+  newEl.className = 'skill';
+  newEl.innerHTML = skill;
+  el.appendChild(newEl);
+});
+
+console.log(el);
