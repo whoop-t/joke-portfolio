30162d5c5cd09b13e98c67a93c949c98fbaf3e6a
whoop-t <tommymallis@gmail.com>
2022-02-09 23:14:07 -0800

    Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,22 +1,25 @@
 const skills = [
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Angular',
-  'Nextjs',
-  'Redux',
-  'Node/Express',
-  'MongoDB',
-  'HTML',
-  'CSS/SCSS/JSS',
-  'BootStrap',
-  'Material'
+	'JavaScript',
+	'TypeScript',
+	'React',
+	'Angular',
+	'Nextjs',
+	'Redux',
+	'Node/Express',
+	'Ruby',
+	'Rails',
+	'MongoDB',
+	'SQL/Postgres',
+	'HTML',
+	'CSS/SCSS/JSS',
+	'BootStrap',
+	'Material'
 ];
 
 const el = document.getElementById('skills-wrapper');
 skills.forEach(skill => {
-  let newEl = document.createElement('div');
-  newEl.className = 'skill';
-  newEl.innerHTML = skill;
-  el.appendChild(newEl);
+	let newEl = document.createElement('div');
+	newEl.className = 'skill';
+	newEl.innerHTML = skill;
+	el.appendChild(newEl);
 });
